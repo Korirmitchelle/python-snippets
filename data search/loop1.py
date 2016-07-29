dd12efcf2a83f2185be5a9d922dc22e73f35d078
Korirmitchelle <korirmitchelle@gmail.com>
2016-07-29 07:18:10 +0300

    user input and output

---
+++ data search/loop1.py
@@ -0,0 +1,15 @@
+#!usr/bin/python
+
+test=raw_input("What is your favourite color")
+if test=="yellow":
+    print("Good, I think we can be friends")
+elif test=="blue":
+    print("Good, nice choice of color")
+elif test=="green":
+    print("Good, mine too")
+elif test=="red":
+    print("Hmm I dont like that though good")
+elif test=="purple":
+    print("nice choice of color")
+else:
+    print("Seriously!!That's not a color")
